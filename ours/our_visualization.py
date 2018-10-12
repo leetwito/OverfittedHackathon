bcd51c7e009c224e76b2aecfa2420f4fe2ac81ba
shlomi <shlomi.medalion@gmail.com>
2018-10-12 16:58:03 +0300

    before final

--- ours/our_visualization.py
+++ ours/our_visualization.py
@@ -16,28 +16,29 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 # In[2]:
 
 
-idx = 33
+idx = 185
 filename = '0'*(7-len(str(idx)))+str(idx)+"_labels.csv"
 filename
 
 
-# In[3]:
+# In[11]:
 
 
-pcw = pd.read_csv("voxelling_output/World2/Train/vid_1/"+filename.replace("labels", "pointcloud"), header=None)/100
-pred = pd.read_csv("voxelling_output/submission_files/vid_1_pred/"+filename, header=None)
-gt = pd.read_csv("voxelling_output/submission_files/vid_1_gt/"+filename, header=None)
+filename = "0000010"
+pcw = pd.read_csv("voxelling_output/submission_files/checks/%s_pointcloud.csv"%filename, header=None)/100
+pred = pd.read_csv("voxelling_output/submission_files/checks/%s_labels.csv"%filename, header=None)
+gt = pd.read_csv("voxelling_output/submission_files/checks/"%filename, header=None)
 
 
-# In[4]:
+# In[9]:
 
 
 # labeled_pc
-pcw["gt"] = gt.values
+pcw["gt"] = pred.values
 pcw = pcw.values
 
 
-# In[5]:
+# In[10]:
 
 
 mask = np.zeros(pcw.shape[0]).astype(int)
